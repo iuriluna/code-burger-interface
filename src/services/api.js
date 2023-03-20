149db0ee833cd4b414ddce47c9a4671101dae7b2
iuriluna <iurideluna@gmail.com>
2023-03-20 09:46:58 -0300

    mudança na porta

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
     const apiCodeBurger = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://api-code-burger.vercel.app'
 })
 
 apiCodeBurger.interceptors.request.use( async config => {
